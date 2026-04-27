0c9c033d769e048695b03e44f6af69aa381773b2
johngrantuk <johngrantuk@googlemail.com>
2026-04-27 20:41:43 +0100

    Add releaseTransparency features

--- data/software-wallets/unrated.tmpl.ts
+++ data/software-wallets/unrated.tmpl.ts
@@ -115,6 +115,15 @@ export const unratedTemplate: SoftwareWallet = {
 		},
 		transparency: {
 			operationFees: null,
+			releaseTransparency: {
+				artifactSigning: null,
+				dependencyLocking: null,
+				dependencyVulnerabilityScanning: null,
+				hasPublicChangelog: null,
+				hermeticBuilds: null,
+				repositoryChangeControls: null,
+				reproducibleBuilds: null,
+			},
 		},
 	},
 	variants: {
